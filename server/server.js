070032c32a5953f54c2a7a6a90e531728fcfe0e8
Erfanch <erfan.chinif@gmail.com>
2020-08-22 22:36:40 -0700

    final edit

--- server/server.js
+++ server/server.js
@@ -6,10 +6,10 @@ const path = require('path')
 
 app.use(express.static(__dirname + '/../client/dist'));
 
-// app.use(express.json());
+app.use(express.json());
 
 app.get('/sidebar/summary', function (req, res) {
-  axios.get('http://54.176.46.102:3380/sidebar/summary')
+  axios.get('http://13.57.255.31:3380/sidebar/summary')
     .then(response => {
       // console.log(response.data);
       res.send(response.data)
@@ -18,7 +18,7 @@ app.get('/sidebar/summary', function (req, res) {
 })
 
 app.get('/kix', function (req, res) {
-  axios.get('http://ec2-54-176-93-251.us-west-1.compute.amazonaws.com/kix')
+  axios.get('http://ec2-13-52-180-39.us-west-1.compute.amazonaws.com/kix')
     .then(response => {
       // console.log(response.data);
       res.status(200).send(response.data)
@@ -27,43 +27,36 @@ app.get('/kix', function (req, res) {
 })
 
 app.get('/api/reviews', function (req, res) {
-  console.log("hello");
-  axios.get('http://52.14.202.194:3001/api/reviews')
+  axios.get('http://13.59.80.115:3001/api/reviews')
     .then(response => {
-      console.log(response.data);
       res.status(200).send(response.data)
     })
     .catch((response) => {console.log('catch', response.data)})
 })
 
 
-// app.post('/sidebar/summary', (req, res) => {
-  // axios.post(request.body, 'http://54.176.46.102:3380/sidebar/summary')
-  //   .then((response) => { callback(response.data); })
-  //   .catch((response) => { callback(response); });
+app.post('/sidebar/summary', (req, res) => {
+  console.log(req.body)
+  axios.post('http://13.57.255.31:3380/sidebar/summary', req.body)
+    .then((response) => res.send(response.data) )
+    .catch((error) => res.send(error));
 
-// })
+})
+
+app.post('/kix', (req, res) => {
+  console.log(req.body)
+  axios.post('http://ec2-13-52-180-39.us-west-1.compute.amazonaws.com/kix', req.body)
+  .then((response) => res.send(response.data))
+  .catch((error) => res.send(error));
+
+})
+
+app.post('/api/reviews', (req, res) => {
+  axios.post('http://13.59.80.115:3001/api/reviews', req.body)
+    .then((response) => res.send(response.data) )
+    .catch((error) => res.send(error));
+
+})
 
 app.listen(PORT, () => console.log('Server is listening at port', PORT))
 
-
-// queryShoeStyle: (request, callback) => {
-//   // let options = {
-//   //   url: 'http://ec2-54-176-93-251.us-west-1.compute.amazonaws.com/kix',
-//   //   method: 'POST',
-//   //   data: request.body
-//   // };
-//   axios.post(request.body, 'http://ec2-54-176-93-251.us-west-1.compute.amazonaws.com/kix')
-//     .then((response) => { callback(response.data); })
-//     .catch((response) => { callback(response); });
-// },
-// app.post('/sidebar/summary', (req, res) => {
-//   controller.querySidebar(req, (err, data) => {
-//     if (err) {
-//       res.send(err);
-//     } else {
-//       console.log('gotit', data);
-//       res.send(data);
-//     }
-//   });
-// });
